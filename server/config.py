e28527cf478909653968b445bd6f668743a1061a
Mati Joseph <mati.joseph.dev@gmail.com>
2026-04-09 08:57:02 +0300

    Change database connection to use cloud instead of local

--- server/config.py
+++ server/config.py
@@ -2,8 +2,13 @@
 
 import os
 
+from dotenv import load_dotenv  # noqa: F401
+
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_COMPACT = False
+    CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
